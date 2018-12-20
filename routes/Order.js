b7e440e2865b6ee961caa8631422f2ce4a7e8dcb
zSukhoi57 <kratorpedo@gmail.com>
2018-12-20 16:24:09 +0800

    daily

--- routes/Order.js
+++ routes/Order.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 //新增資料：接收資料的post
-<<<<<<< Updated upstream
 router.post('/POST/AddOrder', function(req, res, next) {
   console.log('------------------------==');
   var db = req.con;
@@ -27,32 +26,6 @@ router.post('/POST/AddOrder', function(req, res, next) {
       o_purchaseDate: yyyymmdd,
       o_paymentStatus: "waiting"
   };//random() to prevent duplicate NULL entry!
-=======
-router.post('/POST/AddOrder', function (req, res, next) {
-
-    var db = req.con;
-    //當前時間
-    var timeStamp = new Date().getTime();
-    var currentDate = new Date();
-    var date = currentDate.getDate();
-    if (date < 10) { date = '0' + date; }
-    var month = currentDate.getMonth() + 1; //Be careful! January is 0 not 1
-    if (month < 10) { month = '0' + month; }
-    var year = currentDate.getFullYear();
-    var yyyymmdd = year.toString() + month.toString() + date.toString();
-    console.log('timeStamp', timeStamp, 'yyyymmdd', yyyymmdd, year, month, date);
-
-    var sql = {
-        o_id: req.body.o_symbol + "_" + timeStamp,
-        o_symbol: req.body.o_symbol,
-        o_fromAddress: Math.random().toString(36).substring(2, 15),
-        o_txHash: Math.random().toString(36).substring(2, 15),
-        o_tokenCount: req.body.o_tokenCount,
-        o_fundCount: req.body.o_fundCount,
-        o_purchaseDate: yyyymmdd,
-        o_paymentStatus: "new"
-    };//random() to prevent duplicate NULL entry!
->>>>>>> Stashed changes
 
     console.log(sql);
 
@@ -73,22 +46,16 @@ router.post('/POST/AddOrder', function (req, res, next) {
 
 });
 
-<<<<<<< Updated upstream
 //SELECT SUM(o_tokenCount) AS total FROM htoken.`order` WHERE o_symbol = 'MYRR1701';
 //http://localhost:3000/Order/GET/SumAllOrdersBySymbol
 router.get('/GET/SumAllOrdersBySymbol', function(req, res, next) {
     console.log('------------------------==\n@Order/GET/SumAllOrdersBySymbol');
-=======
-//http://localhost:3000/Order/GET/SumOrderedTokensBySymbol
-router.get('/GET/SumOrderedTokensBySymbol', function (req, res, next) {
->>>>>>> Stashed changes
     var db = req.con;
     console.log('req.query', req.query, 'req.query.symbol', req.query.symbol);
     console.log('req.body', req.body, 'req.body.symbol', req.body.symbol);
     let symbol;
     if (req.body.symbol === undefined) {
         symbol = req.query.symbol;
-<<<<<<< Updated upstream
     } else {symbol = req.body.symbol;}
     var qur = db.query(
         'SELECT SUM(o_tokenCount) AS total FROM htoken.order WHERE o_symbol = ?', symbol, function(err, result) {
@@ -280,27 +247,5 @@ router.get('/GET/SumCompletedOrdersBySymbol', function(req, res, next) {
     });
 });
 
-=======
-    } else { symbol = req.body.symbol; }
-    //SELECT SUM(o_tokenCount) AS total FROM htoken.`order` WHERE o_symbol = 'MYRR1701';
-    var qur = db.query(
-        'SELECT SUM(o_tokenCount) AS total FROM htoken.order WHERE o_symbol = "' + symbol + '"', function (err, result) {
-            if (err) {
-                console.log(err);
-                res.status(400);
-                res.json({
-                    "message": "[Error] Failure :\n" + err
-                });
-            } else {
-                res.status(200);
-                res.json({
-                    "message": "[Success] Success",
-                    "result": result
-                });
-            }
-        });
-
-});
->>>>>>> Stashed changes
 
 module.exports = router;
