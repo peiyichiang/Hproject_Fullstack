dcfc7b219ddc92882310f0449508140e4c7495dc
kappabooom <a0983102547@gmail.com>
2019-01-11 21:03:33 +0800

    registry-getUserCount&getUserInfo

--- routes/registryContractAPI.js
+++ routes/registryContractAPI.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const Web3 = require('web3');
+const web3 = require('web3');
 const Tx = require('ethereumjs-tx');
 var router = express.Router();
 
 //Infura HttpProvider Endpoint
-web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/4d47718945dc41e39071666b2aef3e8d"));
+web3js = new web3(new web3.providers.HttpProvider("https://ropsten.infura.io/v3/4d47718945dc41e39071666b2aef3e8d"));
 
 var userAddr = '0x17200B9d6F3D0ABBEccB0e451f50f7c6ed98b5DB';
 var privateKey = Buffer.from('17080CDFA85890085E1FA46DE0FBDC6A83FAF1D75DC4B757803D986FD65E309C', 'hex');
 var contractAddr = '0x391dace017a97273e1231c7072f6cd9dcd05e798';
 
 const contract = require('../contract/registry.json');
-var registryContract = new web3.eth.Contract(contract.abi, contractAddr);
+var registryContract = new web3js.eth.Contract(contract.abi, contractAddr);
 
 
 router.get('/', function (req, res, next) {
@@ -20,11 +20,28 @@ router.get('/', function (req, res, next) {
 
 router.get('/GET/getOwner', async function (req, res, next) {
     let owner = await registryContract.methods.getOwner().call({ from: userAddr })
+
     res.send({
         owner: owner
     })
 });
 
+router.get('/GET/getUserCount', async function (req, res, next) {
+    let count = await registryContract.methods.getUserCount().call({ from: userAddr })
+
+    res.send({
+        count: count
+    })
+});
+
+router.get('/GET/getUserInfo', async function (req, res, next) {
+    let u_id = "A123"
+    let userInfo = await registryContract.methods.getUserInfo(u_id).call({ from: userAddr })
+
+    res.send({
+        userInfo: userInfo
+    })
+});
 
 
 
