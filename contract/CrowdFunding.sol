926618d8698a7a0ee8d6c497910c3eaf248fef1b
jmh1102 <achmed1923@gmail.com>
2019-01-30 10:11:24 +0800

    put safemath library in crowdsale contract

--- contract/CrowdFunding.sol
+++ contract/CrowdFunding.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.5.3;
 
 import "./Ownable.sol";
-import "./SafeMath.sol";
 
 contract CrowdSale is Ownable{
     using SafeMath for uint256;
@@ -128,5 +127,31 @@ contract CrowdSale is Ownable{
     modifier checkPlatform() {
         require(msg.sender == platformAddress);
         _;
+    }
+}
+
+library SafeMath {
+    function mul(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        if (_a == 0) {
+            return 0;
+        }
+        uint256 c = _a * _b;
+        require(c / _a == _b, "safeMath mul failed");
+        return c;
+    }
+    function div(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        uint256 c = _a / _b;
+        // require(b > 0); // Solidity automatically throws when dividing by 0
+        // require(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+    function sub(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        require(_b <= _a, "safeMath sub failed");
+        return _a - _b;
+    }
+    function add(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        uint256 c = _a + _b;
+        require(c >= _a, "safeMath add failed");
+        return c;
     }
 }
