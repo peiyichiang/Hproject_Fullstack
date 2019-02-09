ba617af1ac53f99b1f7485cc48022dec9fe405d3
鄭智豪 <wowthebob@gmail.com>
2019-02-09 09:34:29 +0800

    1.單片售價改成產品定價 2.貨幣種類改成幣別 3.幣別擺在單片售價右邊 4.PSD(Project Starting Date)專案開始日、新增該欄位 5.FMXAdate為FMA審核的日期FMA審核通過後將日期寫入DB 6.PA date(Platform Auditor date) 平台方審核發布到前端的日期 7.p_releasedate中文改為Token發行日，p_validdate中文改為Token到期日 8.案場規模加上單位(kw) 9.總方形量加上單位(token) 10.租金支付週期 改為 租金支付週期每幾個月 11.預估進度圖(順序改為如下) 台電並聯作業(需要新增：p_TaiPowerApprovalDate) 開始購買日(p_CFSD) 能源局同意備案流程(需要新增：p_BOEApprovalDate) 結束購買日(p_CFED) 併聯試運轉程序(需要新增：p_PVTrialOperationDate) 案場建置完成/電費計費開始(需要新增：p_PVOnGridDate) 12.Platform Auditor退回時可以給備註文字 13.上傳案場照片、顯示案場照片(後台、前台)、最多十張

---
+++ contract/Registry.sol
@@ -0,0 +1,96 @@
+pragma solidity ^0.5.3;
+//pragma experimental ABIEncoderV2;
+
+contract RegistryContract{
+
+    address public owner;
+
+    /**@dev 資料結構 */
+    struct User{
+        string u_id; //user 身分證字號
+        address assetAccount; //user assetContract address
+        address etherAddr; //user ethereum (EOA);
+        uint accountStatus; //用數字分狀態，0=>合法、1=>停權 可能會有多種狀態
+    }
+
+    /**@dev 註冊相關event */
+    event setNewUser(string u_id, address assetAccount, address etherAddr, uint accountStatus, uint256 timestamp);
+    event changeAccountStatus(string u_id, uint accountStatus, uint256 timestamp);
+    event changeEthAddr(string u_id, address assetAccount, address etherAddr, uint accountStatus, uint256 timestamp);
+
+    mapping (string=>User) users;
+    string[] userIndex;
+
+    constructor() public{
+        owner = msg.sender;
+    }
+
+    modifier isOwner(){
+        require(msg.sender == owner,"請確認是否為合約擁有者");
+        _;
+    }
+
+    function getOwner() public view returns (address){
+        return owner;
+    }
+
+    /**@dev 新增user */
+    function registerUser(string memory _u_id, address _assetAccount, address _etherAddr, uint256 _time) public isOwner{
+        users[_u_id].u_id = _u_id;
+        users[_u_id].assetAccount = _assetAccount;
+        users[_u_id].etherAddr = _etherAddr;
+        users[_u_id].accountStatus = 0;
+        userIndex.push(_u_id);
+
+        emit setNewUser(_u_id, _assetAccount, _etherAddr, users[_u_id].accountStatus, _time);
+    }
+
+    /**@dev 設定user的狀態 */
+    function setAccountStatus(string memory _u_id, uint _accountStatus, uint256 _time) public isOwner{
+        users[_u_id].accountStatus = _accountStatus;
+
+        emit changeAccountStatus(_u_id, _accountStatus, _time);
+    }
+
+    /**@dev 設定user的以太帳號 */
+    function setEthAddr(string memory _u_id, address _newEtherAddr, uint256 _time) public isOwner{
+        users[_u_id].etherAddr = _newEtherAddr;
+
+        emit changeEthAddr(users[_u_id].u_id, users[_u_id].assetAccount, _newEtherAddr, users[_u_id].accountStatus, _time);
+    }
+
+    /**@dev 取得user數量 */
+    function getUserCount() public view returns(uint userCount){
+        return userIndex.length;
+    }
+
+    /**@dev get user information */
+    function getUserInfo(string memory _u_id) public isOwner view returns (string memory u_id, address assetAccount, address etherAddr, uint accountStatus) {
+        return(users[_u_id].u_id, users[_u_id].assetAccount, users[_u_id].etherAddr, users[_u_id].accountStatus);
+    }
+
+
+
+
+/**@dev 尚未支援回傳string[] */
+/*
+    //get 所有user
+    function getAllUserInfo() public isOwner view  returns (string[], address[], address[], uint[]){
+        string[] memory u_ids = new string[](userIndex.length);
+        address[] memory assetAccounts = new address[](userIndex.length);
+        address[] memory etherAddrs = new address[](userIndex.length);
+        uint[]    memory accountStatus = new uint[](userIndex.length);
+
+        for (uint i = 0; i < userIndex.length; i++) {
+            User storage user = users[userIndex[i]];
+            u_ids[i] = user.u_id;
+            assetAccounts[i] = user.assetAccount;
+            etherAddrs[i] = user.etherAddr;
+            accountStatus[i] = user.accountStatus;
+        }
+
+        return (u_ids, assetAccounts, etherAddrs, accountStatus);
+    }
+*/
+
+}
