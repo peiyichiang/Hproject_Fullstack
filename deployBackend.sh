319437b4ac01b133a83247d81d77df46a70a800b
鄭智豪 <wowthebob@gmail.com>
2019-02-21 17:30:28 +0800

    correct 127.0.0.1 to custom ip

--- deployBackend.sh
+++ deployBackend.sh
@@ -11,6 +11,7 @@ sed -i 's/localhost/140.119.101.130/g' routes/verify_fail.html
 sed -i 's/localhost/140.119.101.130/g' routes/verify_success.html
 
 sed -i 's/127.0.0.1/140.119.101.130/g' views/*.ejs
+sed -i 's/127.0.0.1/140.119.101.130/g' public/js/SignOut.js
 echo "routes/user.js, verify_fail and verify_success: localhost => custom ip"
 
 echo "script finished"
