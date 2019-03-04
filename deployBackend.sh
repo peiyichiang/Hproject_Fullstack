836d5908f1ad87919a0018219c81bee60f177947
鄭智豪 <wowthebob@gmail.com>
2019-03-04 14:37:58 +0800

    修改deployBackend.sh，批次刪除所有/GET與/POST

--- deployBackend.sh
+++ deployBackend.sh
@@ -12,6 +12,14 @@ sed -i 's/localhost/140.119.101.130/g' routes/verify_success.html
 
 sed -i 's/127.0.0.1/140.119.101.130/g' views/*.ejs
 sed -i 's/127.0.0.1/140.119.101.130/g' public/js/SignOut.js
+
+sed -i 's/\/GET//g' routes/backend_user.js
+sed -i 's/\/POST//g' routes/backend_user.js
+sed -i 's/\/GET//g' routes/Product.js
+sed -i 's/\/POST//g' routes/Product.js
+sed -i 's/\/GET//g' views/*.ejs
+sed -i 's/\/POST//g' views/*.ejs
+
 echo "routes/user.js, verify_fail and verify_success: localhost => custom ip"
 
 echo "script finished"
