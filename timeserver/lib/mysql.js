ac181492f06e59bd108edef03156d938abd42f35
Chiu <dennis456852@gmail.com>
2019-04-14 20:45:33 +0800

    use dotenv

--- timeserver/lib/mysql.js
+++ timeserver/lib/mysql.js
@@ -1,16 +1,17 @@
 var mysql = require("mysql");
 
 var pool = mysql.createPool({
-    host: "140.119.101.130",//outside: 140.119.101.130, else 192.168.0.4 or localhost
-    user: "root",
-    password: "bchub",
-    database: "htoken"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 function getCrowdfundingContractAddress(cb) {
     pool.query('SELECT sc_crowdsaleaddress FROM smart_contracts', function (err, rows) {
         if (err) {
-            console.log(err);
+            print(err);
         }
         cb(rows);
     })
@@ -19,7 +20,7 @@ function getCrowdfundingContractAddress(cb) {
 function getRentContractAddress(cb) {
     pool.query('SELECT sc_rentContractaddress FROM smart_contracts', function (err, rows) {
         if (err) {
-            console.log(err);
+            print(err);
         }
         cb(rows);
     })
@@ -28,7 +29,7 @@ function getRentContractAddress(cb) {
 function getOrderDate(cb) {
     pool.query('SELECT o_id, o_purchaseDate FROM htoken.order WHERE o_paymentStatus = "waiting"', function (err, rows) {
         if (err) {
-            console.log(err);
+            print(err);
         }
         cb(rows);
     })
@@ -37,20 +38,25 @@ function getOrderDate(cb) {
 function getERC721ControllerContractAddress(cb) {
     pool.query('SELECT sc_erc721Controller FROM smart_contracts', function (err, rows) {
         if (err) {
-            console.log(err);
+            print(err);
         }
         cb(rows);
     })
 }
 
 function setOrderExpired(o_id, cb) {
-    pool.query('UPDATE htoken.order SET o_paymentStatus = "expired" WHERE o_id = ?', [[[o_id]]], function(err, result) {
-        if(err) {
-            console.log(err);
+    pool.query('UPDATE htoken.order SET o_paymentStatus = "expired" WHERE o_id = ?', [[[o_id]]], function (err, result) {
+        if (err) {
+            print(err);
         }
         cb(result)
     })
 }
+
+function print(s) {
+    console.log('[timeserver]' + s)
+}
+
 module.exports = {
     getCrowdfundingContractAddress,
     getRentContractAddress,
