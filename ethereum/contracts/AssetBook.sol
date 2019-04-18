681154edd0b4e8256fc3b04ba61ac6e17003b15e
Raymond <raymondlieu.crypto2@gmail.com>
2019-04-18 16:38:51 +0800

    daily

--- ethereum/contracts/AssetBook.sol
+++ ethereum/contracts/AssetBook.sol
@@ -146,22 +146,6 @@ contract AssetBook is MultiSig {
         platformCtrt = _platformCtrt;
     }
 
-<<<<<<< HEAD
-=======
-
-    function addAsset(address assetAddr) external ckIsContract(assetAddr) ckAssetOwner {
-        HCAT721ITF_assetbook hcat721 = HCAT721ITF_assetbook(address(uint160(assetAddr)));
-        bytes32 symbol = hcat721.symbol();
-        require(symbol[0] != 0, "symbol should not be empty");
-        uint balance = hcat721.balanceOf(address(this));
-        require(balance + 1000 > balance, "balance should not overflow on adding 1000");
-
-        assetCindex = assetCindex.add(1);
-        assetIndexToAddr[assetCindex] = assetAddr;
-        addrToAssetIndex[assetAddr] = assetCindex;
-    }
-
->>>>>>> livechain
     /** @dev get assets info: asset symbol and balance on this assetbook’s account
 			assetAddr is the asset contract address */
     function getAsset(uint assetIndex, address assetAddr) public view returns (uint assetIndex_, address assetAddr_, bytes32 symbol, uint balance) {
