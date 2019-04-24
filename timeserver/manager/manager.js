581e7d1c960faebd06564fecef28340ecfe6b084
Chiu <dennis456852@gmail.com>
2019-04-24 21:11:51 +0800

    fix some bugs

--- timeserver/manager/manager.js
+++ timeserver/manager/manager.js
@@ -1,15 +1,15 @@
 // const os = require('os');
-// const path = require('path');
-// const fs = require('fs');
+const path = require('path');
+const fs = require('fs');
 const net = require("net");
 
-const {mysqlPoolQuery, getCrowdFundingCtrtAddr,
-  getIncomeManagerCtrtAddr, getOrderDate,
-  getHCAT721ControllerCtrtAddr,
-  setOrderExpired, setCrowdFundingState} = require('../lib/mysql.js');
+const { mysqlPoolQuery, getCrowdFundingCtrtAddr,
+    getIncomeManagerCtrtAddr, getOrderDate,
+    getHCAT721ControllerCtrtAddr,
+    setOrderExpired, setCrowdFundingState } = require('../lib/mysql.js');
 //const {mysqlPoolQuery} = require('../../app');
 
-const {sendTimeTokenController, sendTimeCFctrt, sendTimeIMctrt} = require('../lib/contractAPI.js');
+const { sendTimeTokenController, sendTimeCFctrt, sendTimeIMctrt } = require('../lib/contractAPI.js');
 
 const portForIncomingTime = 7010;
 let currentCount = 0;
@@ -26,9 +26,12 @@ createServer();
 
 function createServer() {
     const server = net.createServer(c => {
-      console.log('inside net.createServer');
+        console.log('inside net.createServer');
 
         c.on("data", (timeCurrent) => {
+            fs.writeFile(path.resolve(__dirname, '..', 'time.txt'), date, function (err) {
+                if (err) console.error(`寫入時間失敗`)
+            })
             print(timeCurrent);
             sendTimeToOrder(timeCurrent.toString());
             updateCrowdFundingDB(timeCurrent.toString());
@@ -50,53 +53,53 @@ function createServer() {
         print(`server bound`);
         let timeCurrent = 201905200000;
         updateCrowdFundingDB(timeCurrent.toString());
-      });
+    });
 }
 
 
-function updateCrowdFundingDB(timeCurrent){
-  console.log('inside updateCrowdFundingDB()');
-  let pstate1 = "initial";
-  let pstate2 = "funding";
-  let pstate3 = "fundingGoalReached";
-  let symbol, pstateNew;
-  var qur = mysqlPoolQuery(
-    'SELECT * FROM htoken.product WHERE (p_state = ? AND p_CFSD <= '+timeCurrent+') OR (p_state = ? AND p_CFED <= '+timeCurrent+') OR (p_state = ? AND p_CFED <= '+timeCurrent+')', [pstate1, pstate2, pstate3], function (err, result) {
-    const resultLen = result.length;
-    console.log('result length', resultLen);
+function updateCrowdFundingDB(timeCurrent) {
+    console.log('inside updateCrowdFundingDB()');
+    let pstate1 = "initial";
+    let pstate2 = "funding";
+    let pstate3 = "fundingGoalReached";
+    let symbol, pstateNew;
+    var qur = mysqlPoolQuery(
+        'SELECT * FROM htoken.product WHERE (p_state = ? AND p_CFSD <= ' + timeCurrent + ') OR (p_state = ? AND p_CFED <= ' + timeCurrent + ') OR (p_state = ? AND p_CFED <= ' + timeCurrent + ')', [pstate1, pstate2, pstate3], function (err, result) {
+            const resultLen = result.length;
+            console.log('result length', resultLen);
 
-    if (err) {
-      console.log(err);
-    } else if (resultLen != 0) {
-      for (let i in result) {
-        symbol = result[i].p_SYMBOL;
-        console.log('symbol', symbol);
-        if (symbol !== 'undefined' && symbol !== 'null' && symbol.length >=3){
+            if (err) {
+                console.log(err);
+            } else if (resultLen != 0) {
+                for (let i in result) {
+                    symbol = result[i].p_SYMBOL;
+                    console.log('symbol', symbol);
+                    if (symbol !== 'undefined' && symbol !== 'null' && symbol.length >= 3) {
 
-          getCrowdFundingCtrtAddr(symbol, (addrCF) => {
-              console.log('addrCF is found:', addrCF);
-          });
-          if (typeof result[i].sc_crowdsaleaddress !== 'undefined' && result[i].sc_crowdsaleaddress != null) {
-            //sendTimeCFctrt(result[i].sc_crowdsaleaddress, timeCurrent.toString()).then(print)
-          }
-          
-          if(result[i].p_state == pstate1){//if p_state == "initial"
-            pstateNew = 'funding';
-          } else {
-            //if(result[i].p_state == pstate2 || result[i].p_state == pstate3)
-            pstateNew = '';
-          }
-          try {
-            setCrowdFundingState(symbol, result[i].p_state, function () {
-              print(symbol + ' fixed p_state: '+ pstate1+ ' => '+pstateNew);
-            })
-          } catch (error) {
-              print(result[i] + " 格式錯誤");
-          }
-        }
-      }
-    }
-  });
+                        getCrowdFundingCtrtAddr(symbol, (addrCF) => {
+                            console.log('addrCF is found:', addrCF);
+                        });
+                        if (typeof result[i].sc_crowdsaleaddress !== 'undefined' && result[i].sc_crowdsaleaddress != null) {
+                            //sendTimeCFctrt(result[i].sc_crowdsaleaddress, timeCurrent.toString()).then(print)
+                        }
+
+                        if (result[i].p_state == pstate1) {//if p_state == "initial"
+                            pstateNew = 'funding';
+                        } else {
+                            //if(result[i].p_state == pstate2 || result[i].p_state == pstate3)
+                            pstateNew = '';
+                        }
+                        try {
+                            setCrowdFundingState(symbol, result[i].p_state, function () {
+                                print(symbol + ' fixed p_state: ' + pstate1 + ' => ' + pstateNew);
+                            })
+                        } catch (error) {
+                            print(result[i] + " 格式錯誤");
+                        }
+                    }
+                }
+            }
+        });
 }
 
 // function updateCrowdFundingDB(timeCurrent) {
