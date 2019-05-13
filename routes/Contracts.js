015923c02fa81a07258f9c0dad6d2e40d8b2f603
kappabooom <a0983102547@gmail.com>
2019-05-13 22:42:26 +0800

    fix api

--- routes/Contracts.js
+++ routes/Contracts.js
@@ -5,7 +5,6 @@ const PrivateKeyProvider = require("truffle-privatekey-provider");
 const timer = require('../timeserver/api.js')
 const router = express.Router();
 const { sequentialRun } = require('../timeserver/blockchain.js');
-const { addTxnInfoRow, addTxnInfoRowFromObj } = require('../timeserver/mysql');
 
 /*Infura HttpProvider Endpoint*/
 //web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/4d47718945dc41e39071666b2aef3e8d"));
@@ -800,9 +799,7 @@ router.post('/HCAT721_AssetTokenContract/:nftSymbol/mint', async function (req, 
 
 
 //for sequential minting tokens ... if mint amount > max 120 to the same target address, the blockchain minting can only accept 120 at one time, so we need to wait for it to finished before minting some more tokens
-// http://localhost:3030/Contracts/ERC721SPLCContract/Htoken05/mintSequential
 router.post('/HCAT721_AssetTokenContract/:nftSymbol/mintSequential', async function (req, res, next) {
-    return;
     let contractAddr = req.body.erc721address;
     let to = req.body.assetBookAddr;
     const amount = parseInt(req.body.amount);
@@ -899,51 +896,6 @@ router.get('/HCAT721_AssetTokenContract/:tokenSymbol/tokenId', async function (r
 
     });
 });
-
-
-router.post('/HCAT721_AssetTokenContract/safeTransferFromBatch', async function (req, res, next) {
-  const contractAddr = req.body.erc721address;
-  const _from = req.body.from;
-  const to = req.body.assetBookAddr;
-  const amount = req.body.amount;
-  const price = req.body.price;
-  const serverTime = req.body.serverTime;
-
-  const tokenSymbol = req.body.tokenSymbol;
-
-  const inst_HCAT721 = new web3.eth.Contract(HCAT721_AssetTokenContract.abi, contractAddr);
-  let encodedData = inst_HCAT721.methods.safeTransferFromBatch(_from, to, amount, price, serverTime).encodeABI();
-  //safeTransferFromBatch(address _from, address _to, uint amount, uint price, uint serverTime)
-  let TxResult = await signTx(backendAddr, backendRawPrivateKey, contractAddr, encodedData);
-
-
-  console.log('after safeTransferFromBatch() is completed...');
-  const txid = tokenSymbol+period+tokenId+txCount
-  const tokenId = tokenId;
-  const txCount = txCount;
-  const holdingDays = holdingDays;
-  const txTime = txTime;
-  const balanceOffromassetbook = balanceOffromassetbook;
-
-  const fromAssetbook = _from;
-  const toAssetbook = to;
-
-  let success = true;
-  await addTxnInfoRow(txid, tokenSymbol, fromAssetbook, toAssetbook, tokenId, txCount, holdingDays, txTime, balanceOffromassetbook).catch((err) => {
-    console.log('\n[Error @ addTxnInfoRow()]', err)
-    success = false;
-  });
-  if(success){
-    res.send({
-      status: "success"
-    });
-  } else {
-    res.send({
-      status: "fail"
-    });
-  }
-});
-
 
 
 /**@dev incomeManager ------------------------------------------------------------------------------------- */
