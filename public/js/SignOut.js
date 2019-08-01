a958234001d4da679228ee36a366ba4d5efcd130
Chih-Hao <wowthebob@gmail.com>
2019-08-01 17:45:09 +0800

    0801Update
    1.更改設定檔

--- public/js/SignOut.js
+++ public/js/SignOut.js
@@ -3,5 +3,6 @@ SignOutBtn.addEventListener('click',function(event){
     event.preventDefault();
     $.removeCookie('access_token', { path: '/' });
     // $.cookie('access_token',null)
-    window.location.href = 'http://127.0.0.1:3030/BackendUser/BackendUserLogin';
+    window.location.href = "/BackendUser/BackendUserLogin";
+    
 });
--- routes/Product.js
+++ routes/Product.js
@@ -7,6 +7,179 @@ var fs = require('fs');
 const { getTime, asyncForEach } = require('../timeserver/utilities');
 const { addScheduleBatch, editActualSchedule, getIncomeScheduleList, addScheduleBatchFromDB } = require('../timeserver/blockchain.js');
 
+// Web3
+const Web3 = require('web3');
+// HTTP provider
+const web3 = new Web3(new Web3.providers.HttpProvider("ropsten.infura.io/v3/136557225d6a4fdcbaf3f37ea4b31097"));
+var abi = 
+[
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_Hash",
+                "type": "string"
+            },
+            {
+                "name": "_blockNumber",
+                "type": "string"
+            },
+            {
+                "name": "_TxHash",
+                "type": "string"
+            }
+        ],
+        "name": "setDocumentBlockInfo",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_eID",
+                "type": "string"
+            },
+            {
+                "name": "_Hash",
+                "type": "string"
+            },
+            {
+                "name": "_year",
+                "type": "string"
+            },
+            {
+                "name": "_studentName",
+                "type": "string"
+            }
+        ],
+        "name": "setDocumentInfo",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_eID",
+                "type": "string"
+            }
+        ],
+        "name": "vieweIDtoHash",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_Hash",
+                "type": "string"
+            }
+        ],
+        "name": "viewHashToblockNumber",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_Hash",
+                "type": "string"
+            }
+        ],
+        "name": "viewHashToeID",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_Hash",
+                "type": "string"
+            }
+        ],
+        "name": "viewHashTostudentName",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_Hash",
+                "type": "string"
+            }
+        ],
+        "name": "viewHashToTxHash",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_Hash",
+                "type": "string"
+            }
+        ],
+        "name": "viewHashToyear",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+// var address = '0x5d7dBC4003C9fb47693489a22008C5Af41ad16C4';
+var address = '0xc2a687911cb895bee7973328f6eaeafa278580a5';
+
+
 //撈取資料(Platform_Supervisor專用，沒在用)
 router.get('/Product', function (req, res, next) {
     console.log('------------------------==\n@Product/Product:\nreq.query', req.query, 'req.body', req.body);
@@ -1725,6 +1898,18 @@ router.get('/ProductDataBySymbol', function (req, res) {
             }
             /* code = 304? */
         });
+});
+
+//回傳該使用者是否可購買token
+router.get('/eDocument', async function (req, res) {
+    var Contract = await new web3.eth.Contract(abi,address);
+    console.log("123");
+
+    res.status(200);
+    res.json({
+        "message": "非專案開賣時間",
+        "result": "123"
+    });
 });
 
 
