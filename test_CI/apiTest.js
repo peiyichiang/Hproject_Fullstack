5eb453ac875e7e8efb3e3528e2f7cb7a56cda201
徐胤桓 <ivan55660228@gmail.com>
2019-11-04 16:46:39 +0800

    test: api整合測試
    使用者購買＆產品上架＆使用者註冊

--- test_CI/apiTest.js
+++ test_CI/apiTest.js
@@ -3,9 +3,12 @@ const request = require('supertest')(app);
 var mocha = require('mocha');
 var faker = require('faker');
 const should = require('should');
-
-const {asyncForEach} = require('../timeserver/utilities');
-
+const assert = require('assert');
+const {mysqlPoolQueryB} = require('../timeserver/mysql.js');
+const {edit_product, add_product, symbol, total, goal, generateCSV, price, type} = require('./api_product');
+const {addAssetbooksIntoCFC} = require('../timeserver/blockchain.js');
+const {asyncForEach, getLocalTime} = require('../timeserver/utilities');
+let virtualAccount;
 //var describe = mocha.describe;
 //var it = mocha.it;
 const version = "/frontendAPI/v1.0"
@@ -96,14 +99,12 @@ const frontEndUserViewingPages = async() => {
           jwt = res.body.jwt;
         });
     });
-    
-    
   })
 }
 const frontEndUserOrdering = async() => {
   describe('intergration testing of front-end user ordering', async function(){
     this.timeout(1000);  
-    let jwt, canBuy = false, symbol, fundingType;
+    let jwt, canBuy = false;
     before('Login before do something', async function(){
       await request
         .get(version + '/user/UserLogin')
@@ -130,7 +131,7 @@ const frontEndUserOrdering = async() => {
       await  request
         .get(version+'/product/canBuyToken')
         .set('Accept', 'application/json')
-        .query({ JWT: jwt, symbol: 'MYRR1701' })
+        .query({ JWT: jwt, symbol: symbol })
         .expect(200)
         .then(async function(res){
           await res.body.message.should.equal('可購買token');
@@ -144,8 +145,9 @@ const frontEndUserOrdering = async() => {
         .expect(200)
         .then(async function(res){
             res.body.result.should.be.instanceOf(Array);
-            symbol = res.body.result[0].symbol;
-            fundingType = res.body.result[0].fundingType;
+            //symbol = res.body.result[0].symbol;
+            //
+            //fundingType = res.body.result[0].fundingType;
         })
     })
     it('get one products detail', async function(){
@@ -169,10 +171,11 @@ const frontEndUserOrdering = async() => {
           await res.body.message.should.equal('[Success] Success');
         })
     })
+    //tokenCount and buyAmount are the same thing
     it('get order compliance', async function(){
       await request
         .get(version+'/order/CheckOrderCompliance')
-        .query({ JWT: jwt, symbol: symbol, email: 'ivan55660228@gmail.com', fundingType: fundingType, authLevel: "5", tokenCount: 1,  buyAmount: 1, userIdentity: "A128465975", fundCount: 15000})
+        .query({ JWT: jwt, symbol: symbol, email: 'ivan55660228@gmail.com', fundingType: type, authLevel: "5", tokenCount: total,  buyAmount: total, userIdentity: "A128465975", fundCount: price * total})
         .set('Accept', 'application/json')
         .expect(200)
         .then(async function(res){
@@ -184,7 +187,7 @@ const frontEndUserOrdering = async() => {
     it('add order to db', async function(){
       await request
         .post(version+'/order/AddOrder')
-        .send({ JWT: jwt, symbol: symbol, email: 'ivan55660228@gmail.com', fundingType: fundingType, authLevel: "5", tokenCount: 1,  buyAmount: 1, userIdentity: "A128465975", fundCount: 15000})
+        .send({ JWT: jwt, symbol: symbol, email: 'ivan55660228@gmail.com', fundingType: type, authLevel: "5", tokenCount: total,  buyAmount: total, userIdentity: "A128465975", fundCount: price * total})
         .set('Accept', 'application/json')
         .expect(200)
         .then(res => {
@@ -195,11 +198,12 @@ const frontEndUserOrdering = async() => {
     it('get unpaid orders detail', async function(){
       await request
         .get(version+'/order/OrdersByEmail')
-        .query({ JWT: jwt, status: ["waiting"]})
+        .query({ JWT: jwt, status: ['waiting', '']})
         .set('Accept', 'application/json')
         .expect(200)
         .then(async function(res){
-          console.log(res.body);
+          console.log(res.body.result[0].o_bankvirtualaccount);
+          virtualAccount = res.body.result[0].o_bankvirtualaccount;
           await res.body.message.should.equal('[Success] Success');
         })
     });
@@ -210,10 +214,278 @@ const frontEndUserOrdering = async() => {
         .set('Accept', 'application/json')
         .expect(200)
         .then(async function(res){
-          console.log(res.body);
           await res.body.message.should.equal('[Success] Success');
         })
     });
   });
 }
-frontEndUserRegistry();
+const FMNAddProduct = async() => {
+  let token;
+  describe('intergration testing of FMS', async function(){
+    this.timeout(1000);  
+    before('Login before do something', async function(){
+      await request
+        .post('/BackendUser/BackendUserLogin')
+        .set('Accept', 'application/json')
+        .send({ m_id: 'myrronlin@gmail.com', m_password: '123456789' })
+        .expect(302)
+        .then(async function(res){
+          await res.header["set-cookie"].should.not.empty();
+          //jwt = res.body.jwt;
+          token = (res.header["set-cookie"]);
+        });
+    });
+    it('Browse Product By FMN', async function(){
+      await request
+        .get('/product/ProductByFMN')
+        .set('Cookie', token)
+        .expect(200)
+        .then(async function(res){
+          res.text.should.not.equal("請先登入");
+          
+        });
+    });
+    it('Add Product By FMN Pages', async function(){
+      await request
+        .get('/product/AddProductByFMN')
+        .set('Cookie', token)
+        .expect(200)
+        .then(async function(res){
+          res.text.should.not.equal("請先登入");
+        });
+    });
+    it('Add Product By FMN', async function(){
+      generateCSV()
+      await request
+        .post('/product/AddProductByFMN')
+        .send(add_product)
+        .set('Cookie', token)
+        .expect(302)
+        .then(async function(res){
+          //res.text.should.not.equal("請先登入");
+        });
+    });
+    
+    it('Edit Product By FMN Pages', async function(){
+      await request
+        .get('/product/EditProductByFMN')
+        .query({ symbol: symbol})
+        .set('Cookie', token)
+        .expect(200)
+        .then(async function(res){
+          
+          //res.text.should.not.equal("請先登入");
+        });
+    });
+    it('Edit Product By FMN', async function(){
+      await request
+        .post('/product/EditProductByFMN')
+        .send(edit_product)
+        .set('Cookie', token)
+        .expect(302)
+        .then(async function(res){
+          
+          //res.text.should.not.equal("請先登入");
+        });
+    });
+    it('Submit Product By FMN', async function(){
+      await request
+        .get('/product/SetProductCreationByFMN')
+        .query({ symbol: symbol})
+        .set('Cookie', token)
+        .expect(302)
+        .then(async function(res){
+          
+          //res.text.should.not.equal("請先登入");
+        });
+    });
+  })
+}
+const FMSApproveProduct = async() => {
+  describe('intergration testing of FMS', async function(){
+    this.timeout(1000);  
+    before('Login before do something', async function(){
+      await request
+        .post('/BackendUser/BackendUserLogin')
+        .set('Accept', 'application/json')
+        .send({ m_id: 'myrronlins@gmail.com', m_password: '123456789' })
+        .expect(302)
+        .then(async function(res){
+          await res.header["set-cookie"].should.not.empty();
+          token = (res.header["set-cookie"]);
+        });
+    });
+    it('Browse Product By FMS', async function(){
+      await request
+        .get('/product/ProductByFMS')
+        .set('Cookie', token)
+        .expect(200)
+        .then(async function(res){
+          res.text.should.not.equal("請先登入");
+          
+        });
+    });
+    it('Browse Product Detail By FMS', async function(){
+      await request
+        .get('/product/ViewProductDeatil')
+        .set('Cookie', token)
+        .query({ symbol: symbol})
+        .expect(200)
+        .then(async function(res){
+          res.text.should.not.equal("請先登入");
+          
+        });
+    });
+    it('Submit Product By FMS', async function(){
+      await request
+        .get('/product/EditProductByFMS')
+        .query({ symbol: symbol})
+        .set('Cookie', token)
+        .expect(302)
+        .then(async function(res){
+          
+          //res.text.should.not.equal("請先登入");
+        });
+    });
+  });
+};
+const PSPublishProduct = async() => {
+  describe('intergration testing of PS publish product', async function(){
+    this.timeout(1000);  
+    before('Login before do something', async function(){
+      await request
+        .post('/BackendUser/BackendUserLogin')
+        .set('Accept', 'application/json')
+        .send({ m_id: 'Platform_Supervisor', m_password: 'Platform_Supervisor' })
+        .expect(302)
+        .then(async function(res){
+          await res.header["set-cookie"].should.not.empty();
+          token = (res.header["set-cookie"]);
+        });
+    });
+    it('Browse Product By PS', async function(){
+      await request
+        .get('/BackendUser/BackendUser_Platform_Supervisor')
+        .set('Cookie', token)
+        .then(async function(res){
+          res.text.should.not.equal("請先登入");
+          
+        });
+    });
+    it('Browse Product Detail By PS', async function(){
+      await request
+        .get('/product/ViewProductDeatil')
+        .query({ symbol: symbol})
+        .set('Cookie', token)
+        .expect(200)
+        .then(async function(res){
+          res.text.should.not.equal("請先登入");
+          
+        });
+    });
+    it('Add CSV Into DB By PS', async function(){
+      await request
+        .post('/product/IncomeCSV')
+        .set('Cookie', token)
+        .send({ IncomeCSVFilePath: 'public/uploadImgs/product.csv' })
+        .expect(200)
+        .then(async function(res){
+          console.log(res.body);
+        });
+    });
+    
+    it('Deploy Crowdfunding By PS', async function(){
+      await request
+        .post('/contracts/crowdFundingContract/' + symbol)
+        .send({ tokenPrice: price, currency: "NTD", quantityMax: total, fundingGoal: goal, CFSD2: '201910180900', CFED2: '201911241700'})
+        .set('Cookie', token)
+        .expect(200)
+        .then(async function(res){
+          console.log(res.body);
+        });
+    }).timeout(10000);
+    it('Set Prodct State By PS', async function(){
+      await request
+        .post('/product/SetProductStateByPlatformSupervisor')
+        .send({ tokenSymbol: symbol, tokenState: 'funding'})
+        .set('Cookie', token)
+        .expect(200)
+        .then(async function(res){
+          console.log(res.body);
+        });
+    });
+    
+  });
+};
+const makeOrderPaidAndWriteIntoCFC = async() => {
+  describe('Make Order Paid And Write Into Crowdfunding', async function(){
+    this.timeout(1000);  
+    it('Make Order Paid', async function(){
+      var sql = { o_paymentStatus: "paid" };
+      const result = await mysqlPoolQueryB('UPDATE order_list SET ? WHERE o_bankvirtualaccount = ?', [sql, virtualAccount]).catch((err) => {
+      });
+    }).timeout(3000);
+    it('Write Into Crowdfunding', async function(){
+      await addAssetbooksIntoCFC(getLocalTime()).catch((err) => {
+        console.error(`[Error @ addAssetbooksIntoCFC]: ${err}`);
+      });
+    }).timeout(10000);
+    
+  });
+};
+const PSMintToken = async() => {
+  let crowdFundingAddr;
+  describe('intergration testing of PS mint token', async function(){
+    this.timeout(1000);  
+    before('Login before do something', async function(){
+      await request
+        .post('/BackendUser/BackendUserLogin')
+        .set('Accept', 'application/json')
+        .send({ m_id: 'Platform_Supervisor', m_password: 'Platform_Supervisor' })
+        .expect(302)
+        .then(async function(res){
+          await res.header["set-cookie"].should.not.empty();
+          token = (res.header["set-cookie"]);
+        });
+    });
+    it('Browse Product By PS', async function(){
+      await request
+        .get('/BackendUser/BackendUser_Platform_Supervisor')
+        .set('Cookie', token)
+        .then(async function(res){
+          res.text.should.not.equal("請先登入");
+          
+        });
+    });
+    it('Get Product status By PS', async function(){
+      await request
+        .get(`/contracts/crowdFundingContract/${symbol}/status`)
+        .set('Cookie', token)
+        .expect(200)
+        .then(async function(res){
+          res.body.fundingState.should.equal('4');
+          crowdFundingAddr = res.body.crowdFundingAddr;
+        });
+    });
+    it('Mint Token By PS', async function(){
+      let data = await mysqlPoolQueryB('SELECT * FROM product WHERE p_SYMBOL = ?', symbol);
+      await request
+        .post(`/contracts/crowdFundingContract/${symbol}/closeFunding`)
+        .set('Cookie', token)
+        .send({'TimeOfDeployment':parseInt(data.p_releasedate), "TimeTokenValid": parseInt(data.p_validdate), "TimeTokenUnlock": parseInt(data.p_lockuptime), "nftName": symbol, "siteSizeInKW": data.p_size, "maxTotalSupply":data.p_totalrelease, "initialAssetPricing": data.p_pricing, "pricingCurrency": data.p_currency, "IRR20yrx100": parseInt(data.p_irr * 100), "tokenURI": data.p_HCAT721uri, "crowdFundingCtrtAddr":crowdFundingAddr})
+        .expect(200)
+        .then(async function(res){
+          console.log(res.body);
+        });
+    }).timeout(10000);
+  });
+};
+const allFlow = async() => {
+  await FMNAddProduct();
+  await FMSApproveProduct();
+  await PSPublishProduct();
+  await frontEndUserOrdering();
+  await makeOrderPaidAndWriteIntoCFC();
+  await PSMintToken();
+}
+allFlow();
---
+++ test_CI/api_product.js
@@ -0,0 +1,143 @@
+var faker = require('faker');
+var fs = require('fs');
+var es = require('event-stream');
+
+let symbol = faker.name.findName().toUpperCase().substring(0,4) + "1111";
+let total = faker.random.number(1000) + 1000;
+let goal = faker.random.number(1000, total);
+let price = faker.random.number(10000) + 10000
+let type = '2';
+const edit_product = {
+    p_SYMBOL: symbol,
+    p_name: faker.name.findName(),
+    p_location: '台南市新營區新工路23號',
+    p_pricing: price,
+    p_duration: "20",
+    p_currency: 'NTD',
+    p_irr: 5.4,
+    p_releasedate: '201911232359',
+    p_validdate: '203912302359',
+    p_size: "300",
+    p_totalrelease: total,
+    p_fundmanager: 'myrronlin@gmail.com',
+    p_PAdate: '2019-10-24 11:27:11 AM',
+    p_CFSD: '201910180900',
+    p_CFED: '201911241700',
+    p_icon: 'public/uploadImgs/1571287635079_icon_ENIS1901.png',
+    p_OnGridAuditedLetter:
+     'public/uploadImgs/1571287635041_file_銀鏈資產管理有限公司(正和製藥)-併聯審查意見書(無初細步協商) [FAX版].pdf',
+    p_assetdocsHash: '',
+    p_state: 'draft',
+    p_RPT: '80',
+    p_FRP: '10/08',
+    p_PSD: '201910181600',
+    p_FMXAdate: '2019-10-23 11:34:05 AM',
+    p_TaiPowerApprovalDate: '201910050800',
+    p_BOEApprovalDate: '201910250800',
+    p_PVTrialOperationDate: '202001150800',
+    p_PVOnGridDate: '201912310800',
+    p_PANote: '',
+    p_FMSNote: '',
+    p_Image1: 'public/uploadImgs/1571366992648_image1_00-01.jpg',
+    p_Image2: 'public/uploadImgs/1571287635146_image2_施作範圍-2.jpg',
+    p_Image3: 'public/uploadImgs/1571287635178_image3_主建物施作範圍.jpg',
+    p_Image4: 'public/uploadImgs/1571287635202_image4_主建物施作範圍-1.jpg',
+    p_Image5: 'public/uploadImgs/1571287635234_image5_車棚空拍照.jpg',
+    p_Image6: 'public/uploadImgs/1571366662168_image6_施作範圍-1.jpg',
+    p_Image7:
+     'public/uploadImgs/1571370557683_image7_銀鏈資產管理有限公司(正和製藥)-機電圖_01.jpg',
+    p_Image8: '',
+    p_Image9: '',
+    p_Image10: '',
+    p_fundingGoal: goal,
+    p_abortedReason: '321321321',
+    p_csvFIle:
+    'public/uploadImgs/product.csv',
+    p_fundingType: type,
+    p_paidNumber: "300",
+    p_EPCname: '朝羿光電能源',
+    p_Copywriting:
+     '愛因斯坦一號案場位於台南市新營區, 平均年日照量達1,292小時以上。本場地為合格Pics藥廠, 結構與屋況保養皆優良。本場使用969片AUO 310W高效模組, 總裝置量300.39kw, 採股票上市公司逆變器等優質部件, 由具工程實績之EPC廠商責任施作、知名能源廠監工, 將帶給您最安心的長期報酬! HCAT電利超商首發產品, 具高達5.4%之20年內部報酬率(IRR), 好機會請勿錯過! ',
+    p_ContractOut: '201907260800',
+    p_CaseConstruction: '201912200800',
+    p_ElectricityBilling: '201912310800',
+    p_isNewCase: "2",
+    p_NotarizedRentalContract: '',
+    p_ForecastedAnnualIncomePerModule: "1",
+    p_lockuptime: ""
+};
+const add_product = {
+    p_SYMBOL: symbol,
+    p_name: faker.name.findName(),
+    p_location: '台南市新營區新工路23號',
+    p_pricing: faker.random.number(10000) + 10000,
+    p_duration: "20",
+    p_currency: 'NTD',
+    p_irr: 5.4,
+    p_releasedate: '201911232359',
+    p_validdate: '203912302359',
+    p_size: "300",
+    p_totalrelease: total,
+    p_fundmanager: 'myrronlin@gmail.com',
+    p_PAdate: '2019-10-24 11:27:11 AM',
+    p_CFSD: '201910180900',
+    p_CFED: '201911241700',
+    p_icon: 'public/uploadImgs/1571287635079_icon_ENIS1901.png',
+    p_OnGridAuditedLetter:
+     'public/uploadImgs/1571287635041_file_銀鏈資產管理有限公司(正和製藥)-併聯審查意見書(無初細步協商) [FAX版].pdf',
+    p_assetdocsHash: '',
+    p_state: 'draft',
+    p_RPT: '80',
+    p_FRP: '10/08',
+    p_PSD: '201910181600',
+    p_FMXAdate: '2019-10-23 11:34:05 AM',
+    p_TaiPowerApprovalDate: '201910050800',
+    p_BOEApprovalDate: '201910250800',
+    p_PVTrialOperationDate: '202001150800',
+    p_PVOnGridDate: '201912310800',
+    p_PANote: '',
+    p_FMSNote: '',
+    p_Image1: 'public/uploadImgs/1571366992648_image1_00-01.jpg',
+    p_Image2: 'public/uploadImgs/1571287635146_image2_施作範圍-2.jpg',
+    p_Image3: 'public/uploadImgs/1571287635178_image3_主建物施作範圍.jpg',
+    p_Image4: 'public/uploadImgs/1571287635202_image4_主建物施作範圍-1.jpg',
+    p_Image5: 'public/uploadImgs/1571287635234_image5_車棚空拍照.jpg',
+    p_Image6: 'public/uploadImgs/1571366662168_image6_施作範圍-1.jpg',
+    p_Image7:
+     'public/uploadImgs/1571370557683_image7_銀鏈資產管理有限公司(正和製藥)-機電圖_01.jpg',
+    p_Image8: '',
+    p_Image9: '',
+    p_Image10: '',
+    p_fundingGoal: goal,
+    p_abortedReason: '321321321',
+    p_csvFIle:
+     'public/uploadImgs/product.csv',
+    p_fundingType: type,
+    p_paidNumber: "300",
+    p_EPCname: '朝羿光電能源',
+    p_Copywriting:
+     '愛因斯坦一號案場位於台南市新營區, 平均年日照量達1,292小時以上。本場地為合格Pics藥廠, 結構與屋況保養皆優良。本場使用969片AUO 310W高效模組, 總裝置量300.39kw, 採股票上市公司逆變器等優質部件, 由具工程實績之EPC廠商責任施作、知名能源廠監工, 將帶給您最安心的長期報酬! HCAT電利超商首發產品, 具高達5.4%之20年內部報酬率(IRR), 好機會請勿錯過! ',
+    p_ContractOut: '201907260800',
+    p_CaseConstruction: '201912200800',
+    p_ElectricityBilling: '201912310800',
+    p_isNewCase: "2",
+    p_NotarizedRentalContract: '',
+    p_ForecastedAnnualIncomePerModule: "1",
+};
+
+const generateCSV = () => {
+    let newCsv =  fs.createWriteStream('./public/uploadImgs/product.csv');
+    let file =  fs.createReadStream('test_CI/sample.csv')
+    file.pipe(es.split())
+        .pipe(
+            es.mapSync(function(line) {
+            // modify line way you want
+                if(line !== 'ia_SYMBOL,ia_time,ia_actualPaymentTime,ia_Payable_Period_End,ia_Annual_End,ia_wholecase_Principal_Called_back,ia_wholecase_Book_Value,ia_wholecase_Forecasted_Annual_Income,ia_wholecase_Forecasted_Payable_Income_in_the_Period,ia_wholecase_Accumulated_Income,ia_wholecase_Income_Recievable,ia_wholecase_Theory_Value,ia_single_Principal_Called_back,ia_single_Forecasted_Annual_Income,ia_single_Forecasted_Payable_Income_in_the_Period,ia_single_Actual_Income_Payment_in_the_Period,ia_single_Accumulated_Income_Paid,ia_single_Token_Market_Price,ia_assetRecord_status'){
+                    line = symbol + line.substring(8)
+                }
+                if(line !== symbol){
+                    newCsv.write(line+'\n');
+                }
+            }))
+}
+module.exports = {edit_product, add_product, symbol, total, goal, generateCSV, price, type};
