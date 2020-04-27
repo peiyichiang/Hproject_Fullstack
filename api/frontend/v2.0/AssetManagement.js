bb2cd374b3ad874d1b8f7cc7fd061d95a9b65cd3
benbenhello <benben5514@gmail.com>
2020-04-27 11:50:40 +0800

    fix: 更改電廠發電資訊<

--- api/frontend/v2.0/AssetManagement.js
+++ api/frontend/v2.0/AssetManagement.js
@@ -2,16 +2,33 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
 var async = require('async');
-
-router.get('/asset',function (req,res){
+const { getTimeServerTime } = require('../../../timeserver/utilities');
+const powerGenerationdata = {
+    "six": 0,
+    "seven": 0,
+    "eight": 0,
+    "nine": 0,
+    "ten": 0,
+    "eleven": 0,
+    "twelve": 0,
+    "thirteen": 0,
+    "fourteen": 0,
+    "fifteen": 0,
+    "sixteen": 0,
+    "seventeen": 0
+}
+router.get('/asset',async function (req,res){
     console.log("This is asset API")
     //get user information from req.decoded
     //_userName = req.decoded.name;
     //_userEmail = req.decoded.email;
     _userEmail = 'ivan55660228@gmail.com';
+    // _date = getTimeServerTime().then()
+    // console.log(_date)
     //database query
+    const time = await getTimeServerTime() // using await to avoid async problem (function should be async)
     const query = req.frontendPoolQuery;
-    query('asset',[_userEmail]).then((result) => {
+    query('asset',[_userEmail,time]).then((result) => {
         var string=JSON.stringify(result); 
         var data = JSON.parse(string);
         data = formating(data);
@@ -23,9 +40,9 @@ router.get('/asset',function (req,res){
 
 })
 function formating(data){
-    newData = []
+    newData = []                                // the output data for new format
     data.forEach(function(item, index, array){
-        key = Object.keys(item);
+        key = Object.keys(item);                // all the sql result has a key see mysql.js
         if(key=="main"){
             item[key].forEach(function(item){
                 newData.push(item);
@@ -35,8 +52,19 @@ function formating(data){
                 var symbol = item.symbol;
                 id = newData.findIndex(obj => obj.symbol === symbol);
                 delete item.symbol;
-                if(newData[id][key]== undefined) newData[id][key] = [];
+                if(newData[id][key]== undefined) newData[id][key] = [];  // initialize the assetRecord with aan empty arry
                 newData[id][key].push(item);
+            })
+        }else if(key=="powerGeneration"){
+            newData.forEach(function(elm){
+                if(elm[key]== undefined) elm[key] = powerGenerationdata;    // setting powerGeneration default values
+            })
+            item[key].forEach(function(item){
+                // console.log(item);
+                var symbol = item.symbol;
+                newData.forEach(function(elm){
+                    if(elm.symbol == symbol) elm[key] = {...elm[key],...item};  // using spread syntax to overwrite default values
+                })
             })
 
         }    // forEach 就如同 for，不過寫法更容易
