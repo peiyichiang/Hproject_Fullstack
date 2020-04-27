fa85c2939faee4b36e7b50130251f66067ef6895
benbenhello <benben5514@gmail.com>
2020-04-27 15:16:39 +0800

    fix: 去除login router

--- api/frontend/v2.0/index.js
+++ api/frontend/v2.0/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 // router.use('/User', require('./User.js'));
 router.use('/Product', require('./Product.js'));
 router.use('/AssetManagement', require('./AssetManagement.js'));
-router.use('/Login',require('./Login.js'));
+// router.use('/Login',require('./Login.js'));
 router.use('/Order',require('./Order.js'));
 // router.use('/Payment', require('./Payment.js'));
 // router.use('/IncomeManagement', require('./IncomeManagement.js'));
